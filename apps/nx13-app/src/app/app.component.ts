d61f115499fd824eb2d42b66674a063a4b3a5b62
Stefan Schweiger <stefan.schweiger@klaiton.com>
2022-01-04 14:24:04 +0100

    initial commit

---
+++ apps/nx13-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx13-test-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'nx13-app';
+}
